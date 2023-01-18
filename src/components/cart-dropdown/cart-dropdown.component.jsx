c5e1e01ba95153ef205bcbd1fcf5b7cb66f8616a
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-18 15:42:47 +0530

    modified and coverted into styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.contex';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles.jsx';
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
+
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -13,19 +19,17 @@ const CartDropdown = () => {
     };
   
     return (
-      <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-          {cartItems.length ? (
-            cartItems.map((cartItem) => (
-              <CartItem key={cartItem.id} cartItem={cartItem} />
-            ))
-          ) : (
-            <span className='empty-message'>Your cart is empty</span>
-          )}
-        </div>
-        <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-      </div>
-    );
-  };
+      <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
   
   export default CartDropdown;
