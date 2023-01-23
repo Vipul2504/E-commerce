a93ec13fb16e505fb09a873bb270fbd46a41362e
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-23 23:37:31 +0530

    modified

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,30 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
-    (acc, { title, items }) => {
-      acc[title.toLowerCase()] = items;
-      return acc;
-    },
-    {}
-  );
-  return categoriesMap;
-};
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    categoriesSlice.categories
+  }
+)
+
+// export const  = (state) => {
+//   const categoriesMap = state.categories.categories.reduce(
+//     (acc, { title, items }) => {
+//       acc[title.toLowerCase()] = items;
+//       return acc;
+//     },
+//     {}
+//   );
+//   return categoriesMap;
+// };
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const {title, items} = category;
+    acc[title.toLowerCase()] = items;
+    return items;
+  }, {})
+)
