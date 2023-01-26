f01ee090c980453ab46e5ae99276873bbe28dd36
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-26 14:23:10 +0530

    made small chnages

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -3,5 +3,7 @@
   import { CATEGORIES_ACTION_TYPES } from './category.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+ const setCategories = (categoriesArray) =>
+  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesArray);
+
+  export default setCategories;
