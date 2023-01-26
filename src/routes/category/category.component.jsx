9717cd9ee0119e12a44428717970a0e8f626cab7
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-26 16:59:45 +0530

    added spinner component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,12 +4,13 @@ import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoryContainer, Title } from './category.styles';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import { useSelector } from 'react-redux';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectCategoriesIsLoading)
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,12 +20,17 @@ const Category = () => {
   return (
     <>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {
+        isLoading ? (<Spinner />) : (
+          <CategoryContainer>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </CategoryContainer>
+        )
+      }
+
     </>
   );
 };
