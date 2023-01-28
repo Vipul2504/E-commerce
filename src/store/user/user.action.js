295cac848118ea254b77260e2382401c16630019
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-29 01:13:02 +0530

    added signin signout actions

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -18,3 +18,20 @@ export const signInSuccess = (user) =>
 
 export const signInFailled = (error) =>
   createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
+
+export const signUpStart = (email, passoword, displayName) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_START, {
+    email,
+    passoword,
+    displayName,
+  });
+
+export const signUpSuccess = ({ user, additionDetails }) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionDetails });
+
+export const signUpFailled = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
+
+export const signOutStart = () => createAction(USER_ACTION_TYPES.SIGN_OUT_START) 
+export const signOutSuccess = () => createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS)
+export const signOutFailed = (error) => createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error)
