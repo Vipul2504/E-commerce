406333592968b406145aaebda9dfdadf54b27e5f
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-29 01:12:45 +0530

    added signin signout methods

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,32 +1,115 @@
-import { takeLatest, call, all, put } from "redux-saga/effects";
+import { takeLatest, call, all, put, take } from "redux-saga/effects";
 import USER_ACTION_TYPES from "./user.types";
-import { signInSuccess, signInFailed } from "./user.action";
-import { createUserDocumentFromAuth, getCurrentUser } from "../../utils/firebase/firebase.utils";
+import {
+  signInSuccess,
+  signInFailed,
+  signUpSuccess,
+  signUpFailled,
+  signOutSuccess,
+  signOutFailed,
+} from "./user.action";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+  getCurrentUser,
+  signInWithGooglePopUp,
+  signOutUSer,
+} from "../../utils/firebase/firebase.utils";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
+export function* getSnapShotFromUserAuth(userAuth, additionDetails) {
+  try {
+    const userSnapshot = yield call(
+      createUserDocumentFromAuth,
+      userAuth,
+      additionDetails
+    );
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {}
+}
 
-export function* getSnapShotFromUserAuth(userAuth, additionDetails){
-    try {
-        const userSnapshot = yield call(createUserDocumentFromAuth,userAuth, additionDetails);
-        yield put(signInSuccess({id:userSnapshot.id, ...userSnapshot.data()}))
-    } catch (error) {
-        
-    }
+export function* signWithGoogle() {
+  try {
+    const { user } = yield call(signInWithGooglePopUp);
+    yield call(getSnapShotFromUserAuth, user);
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield call(signInWithEmailAndPassword, email, password);
+    yield call(getSnapShotFromUserAuth, user);
+  } catch (error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield call(
+      createAuthUserWithEmailAndPassword,
+      email,
+      password
+    );
+    yield put(signUpSuccess(user, { displayName }));
+  } catch (error) {
+    yield put(signUpFailled(error));
+  }
 }
 
 export function* isUserAuthenticated() {
-    try {
-        const userAuth = yield all(getCurrentUser);
-        if(!userAuth) return
-        yield call(getSnapShotFromUserAuth, userAuth)
-    } catch (error) {
-        
-    }
+  try {
+    const userAuth = yield all(getCurrentUser);
+    if (!userAuth) return;
+    yield call(getSnapShotFromUserAuth, userAuth);
+  } catch (error) {}
 }
 
-export function* onCheckUserSession(){
-    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated)
+export function* signOut() {
+  try {
+    yield call(signOutUSer);
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailed(error));
+  }
 }
 
-export function* userSaga(){
-    yield all([call(onCheckUserSession)])
+export function* signInAfterSignUp({ payload: { user, additionDetails } }) {
+  yield call(getSnapShotFromUserAuth, user, additionDetails);
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signWithGoogle);
+}
+
+export function* onEmailSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
+export function* onCheckUserSession() {
+  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* onSignUpStart() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSucesss() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
+export function* onSignOutStart() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+}
+
+export function* userSaga() {
+  yield all([
+    call(onCheckUserSession),
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(signUpSuccess),
+    call(onSignOutStart)
+  ]);
 }
