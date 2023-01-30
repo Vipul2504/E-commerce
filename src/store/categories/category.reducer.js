49c6f944724b8688a745399b7eddcff34cdf47ce
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-30 17:16:44 +0530

    added redux-saga and functions to fecth

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,9 +1,9 @@
-import {CATEGORIES_ACTION_TYPES} from './category.types';
+import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
-  isLoading : false,
-  error : null,
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReducer = (
@@ -14,12 +14,11 @@ export const categoriesReducer = (
 
   switch (type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {...state, isLoading: true}
+      return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, isLoading:false, categories: payload };
+      return { ...state, categories: payload, isLoading: false };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return {...state, error:payload, isLoading:false}
-      
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
