d46ddaf42200c809bff4b6977d2dc10aac6eb537
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-02 22:55:41 +0530

    modified

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,6 @@
 import { Outlet} from "react-router-dom";
 import "./navigation.styles"
 import { ReactComponent as CrwnLogo}  from '../../assets/crown.svg';
-import { signOutUSer } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { useDispatch, useSelector } from "react-redux";
