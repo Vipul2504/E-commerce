c19e6635ce65126976c3cd7ea3e7f022d7e994c0
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-09 14:59:35 +0530

    modified

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -3,3 +3,5 @@ import { loadStripe } from "@stripe/stripe-js";
 export const stripePromise = loadStripe(
   process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
 );
+
+console.log(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
