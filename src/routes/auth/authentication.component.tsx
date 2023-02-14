28a9df3336564d8758e03fe62611383f21c4852f
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-14 17:00:00 +0530

    coverted into TypeScript

---
+++ src/routes/auth/authentication.component.tsx
@@ -0,0 +1,14 @@
+import SignInForm from "../sign-in-form/sign-in-form.component";
+import SignUpForm from "../sign-up-form/sign-up-form.component";
+import { AuthenticationContainer } from './authentication.styles';
+
+const Authentication = () => {
+    return (
+        <AuthenticationContainer>
+        <SignInForm />
+        <SignUpForm />
+      </AuthenticationContainer>
+    )
+}
+
+export default Authentication;
