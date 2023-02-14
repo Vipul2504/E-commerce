d5c79573883878b1423844e12f194515e7829bc3
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-14 16:54:54 +0530

    coverted into TypeScript

---
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -0,0 +1,32 @@
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  selectCartCount,
+  selectIsCartOpen,
+} from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+
+import './cart-icon.styles';
+import { CartIconContainer, ItemCount } from './cart-icon.styles';
+
+const CartIcon = () => {
+  const dispatch = useDispatch();
+  const isCartOpen = useSelector(selectIsCartOpen);
+  const cartCount = useSelector(selectCartCount);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
+};
+
+export default CartIcon;
+
